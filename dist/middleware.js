9cca6815e3a0e3667aecd9fc38e6ae09bbcbef8e
Thomas Conner <thomas@kinvey.com>
2016-09-21 10:28:33 -0400

    1.1.0

---
+++ dist/middleware.js
@@ -0,0 +1,78 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Middleware = undefined;
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _asciitree = require('./asciitree');
+
+var _regeneratorRuntime = require('regenerator-runtime');
+
+var _regeneratorRuntime2 = _interopRequireDefault(_regeneratorRuntime);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// eslint-disable-line no-unused-vars
+
+var Middleware = exports.Middleware = function () {
+  function Middleware() {
+    var name = arguments.length <= 0 || arguments[0] === undefined ? 'Middleware' : arguments[0];
+
+    _classCallCheck(this, Middleware);
+
+    this.name = name;
+  }
+
+  _createClass(Middleware, [{
+    key: 'handle',
+    value: function () {
+      var _ref = _asyncToGenerator(_regeneratorRuntime2.default.mark(function _callee() {
+        return _regeneratorRuntime2.default.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                throw new Error('A subclass middleware must override the handle function.');
+
+              case 1:
+              case 'end':
+                return _context.stop();
+            }
+          }
+        }, _callee, this);
+      }));
+
+      function handle() {
+        return _ref.apply(this, arguments);
+      }
+
+      return handle;
+    }()
+  }, {
+    key: 'generateTree',
+    value: function generateTree() {
+      var level = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
+
+      var root = {
+        value: this.name,
+        level: level,
+        nodes: []
+      };
+      return root;
+    }
+  }, {
+    key: 'toString',
+    value: function toString() {
+      var root = this.generateTree();
+      return _asciitree.AsciiTree.generate(root);
+    }
+  }]);
+
+  return Middleware;
+}();
